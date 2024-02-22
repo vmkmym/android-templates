9b3007c8a1b505331b8c2a37249a8ba7f6a6fcd2
상은 <71699054+vmkmym@users.noreply.github.com>
2024-02-22 21:41:58 +0900

    feat: Crossfade를 이용한 이미지 변경 애니메이션 구현
    - ChangeImageAnimation 컴포저블을 MainActivity에서 직접 호출하는 대신 별도의 @Composable 함수를 만들어서 UI를 구성 -> UI코드 모듈화, 재사용성 향상
    
    - 앱의 생명주기에 따라 UI를 더 효과적으로 관리하기 위해 onStart 메서드에서 setContent 호출 (또는 onResume)
    
    - Button의 onClick 이벤트 핸들러로 함수를 직접 전달하면 코드가 간결해지고 가독성이 향상됨 -> ::previousImage와 ::nextImage는 각각 previousImage와 nextImage 함수에 대한 참조

--- app/src/main/java/com/example/composeproject/MainActivity.kt
+++ app/src/main/java/com/example/composeproject/MainActivity.kt
@@ -3,18 +3,35 @@ package com.example.composeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeproject.ui.theme.ComposeProjectTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+    override fun onStart() {
+        super.onStart()
         setContent {
             ComposeProjectTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +39,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -30,17 +47,70 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MainScreen() {
+    ChangeImageAnimation()
+}
+
+@Composable
+fun ChangeImageAnimation() {
+    val images = listOf(
+        R.drawable.img1,
+        R.drawable.img2,
+        R.drawable.img3,
+        R.drawable.img4,
+        R.drawable.img5,
+        R.drawable.img6
     )
+    var currentImageIndex by remember { mutableIntStateOf(0) }
+
+    fun nextImage() {
+        currentImageIndex = (currentImageIndex + 1) % images.size
+    }
+
+    fun previousImage() {
+        currentImageIndex = (currentImageIndex - 1 + images.size) % images.size
+    }
+
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        // 3. Crossfade 컴포저를 사용하여 이미지를 교체하는 애니메이션을 적용
+        Crossfade(
+            targetState = currentImageIndex,
+            label = "Crossfade between images"
+        ) { index ->
+            Image(
+                painter = painterResource(id = images[index]),
+                contentDescription = "Image ${index + 1} out of ${images.size}",
+                modifier = Modifier.size(300.dp)
+            )
+        }
+        Row(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+        ) {
+            Button(
+                // 1. 버튼을 클릭하면 previousImage 함수가 호출되도록 설정
+                onClick = ::previousImage,
+                modifier = Modifier.width(80.dp)
+            ) {
+                Text("이전")
+            }
+            Button(
+                // 2. 버튼을 클릭하면 nextImage 함수가 호출되도록 설정
+                onClick = ::nextImage,
+                modifier = Modifier.width(80.dp)
+            ) {
+                Text("다음")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeProjectTheme {
-        Greeting("Android")
+        ChangeImageAnimation()
     }
 }
