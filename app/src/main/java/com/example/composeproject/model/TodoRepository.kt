77e7297426cc0d8939f26fb56601c74aefddf2c2
상은 <71699054+vmkmym@users.noreply.github.com>
2024-03-25 02:08:23 +0900

    feat: add TodoRepository

---
+++ app/src/main/java/com/example/composeproject/model/TodoRepository.kt
@@ -0,0 +1,27 @@
+package com.example.composeproject.model
+
+import kotlinx.coroutines.flow.Flow
+
+// Declares the DAO as a private property in the constructor. Pass in the DAO
+// instead of the whole database, because you only need access to the DAO
+class TodoRepository(private val todoDao: TodoDao) {
+
+    suspend fun addTodo(todoItem: TodoItem) {
+        todoDao.addTodo(todoItem)
+    }
+
+    fun getTodos(): Flow<List<TodoItem>> = todoDao.getAllTodos()
+
+    fun getATodoById(id: Long): Flow<TodoItem> {
+        return todoDao.getATodoById(id)
+    }
+
+    suspend fun updateTodo(todoItem: TodoItem) {
+        todoDao.updateTodo(todoItem)
+    }
+
+    suspend fun deleteTodo(todoItem: TodoItem) {
+        todoDao.deleteTodo(todoItem)
+    }
+
+}
