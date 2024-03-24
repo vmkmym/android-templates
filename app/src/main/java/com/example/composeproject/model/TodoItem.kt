8fc354d8757674b822b3a5f16d8d4b67b750fbfa
상은 <71699054+vmkmym@users.noreply.github.com>
2024-03-25 02:08:44 +0900

    feat: add TodoItem

--- app/src/main/java/com/example/composeproject/model/TodoItem.kt
+++ app/src/main/java/com/example/composeproject/model/TodoItem.kt
@@ -1,18 +1,43 @@
 package com.example.composeproject.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
+@Entity(tableName = "todo_table")
+@TypeConverters(DateConverter::class)
 data class TodoItem(
-    val title: String = "",
-    val description: String = "",
-    var isCompleted: Boolean = false,
-    val isCreatedDate: Date = Date(),
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
+    @ColumnInfo(name = "todo-title")
+    val title: String,
+    @ColumnInfo(name = "todo-desc")
+    val description: String,
+    @ColumnInfo(name = "date")
+    val date: Date,
+    @ColumnInfo(name = "is-completed")
+    var isCompleted: Boolean = false
 ) {
     val formattedDate: String
         get() {
             val format = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
-            return format.format(isCreatedDate)
+            return format.format(date)
         }
+}
+
+class DateConverter {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time?.toLong()
+    }
 }
