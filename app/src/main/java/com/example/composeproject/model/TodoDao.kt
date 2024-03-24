0b690553c1ccb4b082255508101ad90f4c84cf5f
상은 <71699054+vmkmym@users.noreply.github.com>
2024-03-25 02:09:10 +0900

    feat: add TodoDao

---
+++ app/src/main/java/com/example/composeproject/model/TodoDao.kt
@@ -0,0 +1,29 @@
+package com.example.composeproject.model
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TodoDao {
+    // https://developer.android.com/training/data-storage/room/async-queries?hl=ko#flow-coroutines
+    // Loads all todos from the todo_table
+    @Query("SELECT * FROM todo_table ORDER BY id ASC")
+    fun getAllTodos(): Flow<List<TodoItem>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addTodo(todoItem: TodoItem)
+
+    @Update
+    suspend fun updateTodo(todoItem: TodoItem)
+
+    @Delete
+    suspend fun deleteTodo(todoItem: TodoItem)
+
+    @Query("Select * from todo_table where id=:id")
+    fun getATodoById(id:Long): Flow<TodoItem>
+}
