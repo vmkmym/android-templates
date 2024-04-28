9b20c276d1044631e362abb638a9a15c9d4eeb79
상은 <71699054+vmkmym@users.noreply.github.com>
2024-04-29 02:21:41 +0900

    feat: 우선 순위 드랍다운 기능 구현, 마감 기한 버튼 리팩토링 및 함수화, 세부 UI 수정

--- app/src/main/java/com/example/composeproject/view/TodoListScreen.kt
+++ app/src/main/java/com/example/composeproject/view/TodoListScreen.kt
@@ -5,6 +5,7 @@ package com.example.composeproject.view
 import android.app.DatePickerDialog
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -17,14 +18,20 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MenuDefaults
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SheetState
@@ -33,9 +40,9 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -62,6 +69,8 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
 import java.util.Locale
+
+//TODO: 정렬 버튼을 누르면 생성일 기준, 마감기한 내림차순, 마감기한 오름차순, 우선순위 내림차순, 우선순위 오름차순. 만약 마감기한이나 우선순위 기준으로 정렬을 했는데 해당값이 없음이면, 생성일 기준으로 대체.
 
 @Composable
 @ExperimentalMaterial3Api
@@ -119,16 +128,6 @@ private fun ModalSheet(
     onDismissRequest: () -> Unit
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
-    var selectedDate by remember { mutableStateOf<Date?>(null) }
-    var selectedTodo by remember { mutableStateOf<TodoItem?>(null) }
-
-    if (selectedDate == null) {
-        SelectYearMonthDay { year, month, day ->
-            val calendar = Calendar.getInstance()
-            calendar.set(year, month - 1, day)
-            selectedDate = calendar.time
-        }
-    }
 
     ModalBottomSheet(
         sheetState = sheetState,
@@ -140,7 +139,11 @@ private fun ModalSheet(
             }
         },
         content = {
-            Column {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            ) {
                 TextField(
                     value = viewModel.todoTitleState,
                     onValueChange = viewModel::onTodoTitleChanged,
@@ -167,21 +170,19 @@ private fun ModalSheet(
                     modifier = Modifier.fillMaxWidth(),
                     keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done)
                 )
+
+                // 우선 순위 드랍다운 버튼
+                PriorityDropdownButton()
+
+                // 마감 기한 버튼
+                DeadlineButton(viewModel)
+
                 Row(
                     modifier = Modifier
-                        .align(Alignment.End)
-                        .padding(top = 16.dp, bottom = 16.dp, start = 8.dp, end = 8.dp),
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    // 마감기한을 선택하는 버튼 (사용자 선택사항)
-                    Button(
-                        onClick = {
-                            viewModel.dueDateState = selectedDate
-                            selectedDate = null
-                        }
-                    ) {
-                        Text("마감기한 선택")
-                    }
-                    Spacer(modifier = Modifier.padding(end = 8.dp))
                     Button(
                         onClick = {
                             scope.launch {
@@ -205,18 +206,18 @@ private fun ModalSheet(
                             Text("수정")
                         }
                     } else {
-                    Button(
-                        onClick = {
-                            viewModel.addCurrentTodo()
-                            scope.launch {
-                                keyboardController?.hide()
-                                sheetState.hide()
+                        Button(
+                            onClick = {
+                                viewModel.addCurrentTodo()
+                                scope.launch {
+                                    keyboardController?.hide()
+                                    sheetState.hide()
+                                }
                             }
+                        ) {
+                            Text("확인")
                         }
-                    ) {
-                        Text("확인")
                     }
-                }
                 }
             }
         }
@@ -248,9 +249,7 @@ fun TodoListCard(
                 } else {
                     viewModel.onTodoDeselected(todoItem)
                 }
-            },
-            modifier = Modifier
-                .padding(top = 5.dp, start = 5.dp, end = 5.dp)
+            }
         )
         TodoList(it = todoItem)
     }
@@ -259,47 +258,60 @@ fun TodoListCard(
 
 @Composable
 private fun TodoList(it: TodoItem) {
-    Column(
-        modifier = Modifier
-            .padding(start = 10.dp, bottom = 10.dp)
+    val priorities = listOf(" ", "!", "!!", "!!!")
+    val index = it.priority % priorities.size
+    val priorityText = priorities[index]
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            text = it.title,
+            text = priorityText,
             style = TextStyle(
-                color = Color.Black,
-                fontSize = 16.sp,
+                color = Color.Red,
+                fontSize = 14.sp,
                 fontFamily = FontFamily(Font(R.font.jetbrains))
             )
         )
-
-        Text(
-            text = it.description,
-            style = TextStyle(
-                color = Color.DarkGray,
-                fontSize = 13.sp,
-                fontFamily = FontFamily(Font(R.font.jetbrains))
-            )
-        )
-        Row {
+        Column {
             Text(
-                text = it.formattedDate,
+                text = it.title,
                 style = TextStyle(
-                    color = Color.LightGray,
-                    fontSize = 10.sp,
+                    color = Color.Black,
+                    fontSize = 16.sp,
                     fontFamily = FontFamily(Font(R.font.jetbrains))
                 )
             )
-            it.dueDate?.let { dueDate ->
-                val format = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
-                val formattedDueDate = format.format(dueDate)
+            Text(
+                text = it.description,
+                style = TextStyle(
+                    color = Color.DarkGray,
+                    fontSize = 13.sp,
+                    fontFamily = FontFamily(Font(R.font.jetbrains))
+                )
+            )
+            Row {
                 Text(
-                    text = "   마감일: $formattedDueDate",
+                    text = "생성일: ${it.formattedDate}",
                     style = TextStyle(
-                        color = Color.Blue,
+                        color = Color.LightGray,
                         fontSize = 10.sp,
                         fontFamily = FontFamily(Font(R.font.jetbrains))
                     )
                 )
+                it.dueDate?.let { dueDate ->
+                    val format = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
+                    val formattedDueDate = format.format(dueDate)
+                    Text(
+                        text = "   마감일: $formattedDueDate",
+                        style = TextStyle(
+                            color = Color.LightGray,
+                            fontSize = 10.sp,
+                            fontFamily = FontFamily(Font(R.font.jetbrains))
+                        )
+                    )
+                }
             }
         }
     }
@@ -341,20 +353,136 @@ fun transparentTextFieldColors() = TextFieldDefaults.textFieldColors(
 )
 
 @Composable
-fun SelectYearMonthDay(onDateSelected: (year: Int, month: Int, day: Int) -> Unit) {
+fun PriorityDropdown(
+    priorities: List<String>,
+    selectedPriorityIndex: Int,
+    expanded: Boolean,
+    onExpandedChanged: (Boolean) -> Unit,
+    onPrioritySelected: (Int) -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .clickable { onExpandedChanged(!expanded) }
+    ) {
+        Row {
+            Text(
+                text = priorities[selectedPriorityIndex],
+                style = TextStyle(
+                    color = Color.LightGray,
+                    fontSize = 16.sp,
+                    fontFamily = FontFamily(Font(R.font.jetbrains))
+                )
+            )
+            Icon(Icons.Filled.KeyboardArrowRight, contentDescription = "우선순위 선택")
+        }
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { onExpandedChanged(false) },
+        ) {
+            priorities.forEachIndexed { index, priority ->
+                DropdownMenuItem(
+                    text = { Text(priority) },
+                    onClick = { onPrioritySelected(index) },
+                    modifier = Modifier.padding(5.dp),
+                    leadingIcon = {},
+                    trailingIcon = {},
+                    colors = MenuDefaults.itemColors(),
+                    contentPadding = MenuDefaults.DropdownMenuItemContentPadding,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun PriorityDropdownButton() {
+    val priorities = listOf("없음", "낮음", "중간", "높음")
+    var selectedPriorityIndex by remember { mutableIntStateOf(0) }
+    var expanded by remember { mutableStateOf(false) }
+    val onExpandedChanged = { newExpanded: Boolean ->
+        expanded = newExpanded
+    }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(7.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Icon(Icons.Filled.Info, contentDescription = "우선순위 아이콘")
+            Spacer(modifier = Modifier.padding(5.dp))
+            Text(
+                text = "우선 순위",
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = 16.sp
+                )
+            )
+        }
+        PriorityDropdown(
+            priorities = priorities,
+            selectedPriorityIndex = selectedPriorityIndex,
+            expanded = expanded,
+            onExpandedChanged = onExpandedChanged,
+            onPrioritySelected = { index ->
+                selectedPriorityIndex = index
+                onExpandedChanged(false)
+            }
+        )
+    }
+}
+
+@Composable
+fun DeadlineButton(viewModel: TodoViewModel) {
+    var selectedDate by remember { mutableStateOf<Date?>(null) }
     val context = LocalContext.current
     val calendar = Calendar.getInstance()
     val year = calendar.get(Calendar.YEAR)
     val month = calendar.get(Calendar.MONTH)
     val day = calendar.get(Calendar.DAY_OF_MONTH)
 
-    LaunchedEffect(key1 = true) {
-        val datePickerDialog =
-            DatePickerDialog(context, { _, selectedYear, selectedMonth, selectedDay ->
-                onDateSelected(selectedYear, selectedMonth + 1, selectedDay)
-            }, year, month, day)
-
-        datePickerDialog.datePicker.minDate = calendar.timeInMillis
-        datePickerDialog.show()
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(7.dp)
+            .clickable { // 클릭 시 캘린더를 표시
+                DatePickerDialog(context, { _, selectedYear, selectedMonth, selectedDay ->
+                    calendar.set(selectedYear, selectedMonth, selectedDay)
+                    selectedDate = calendar.time
+                    viewModel.dueDateState = selectedDate
+                }, year, month, day).show()
+            },
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Icon(Icons.Filled.DateRange, contentDescription = "마감기한 아이콘")
+            Spacer(modifier = Modifier.padding(5.dp))
+            Text(
+                text = "마감 기한",
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = 16.sp
+                )
+            )
+        }
+        Row {
+            Text(
+                /*선택한 마감 날짜를 보이게 함. 마감일 선택 안하면 없음이 기본값*/
+                text = selectedDate?.let {
+                    SimpleDateFormat("yyyy.MM.dd", Locale.getDefault()).format(it)
+                } ?: "없음",
+                style = TextStyle(
+                    color = Color.LightGray,
+                    fontSize = 16.sp,
+                    fontFamily = FontFamily(Font(R.font.jetbrains))
+                )
+            )
+            Icon(Icons.Filled.KeyboardArrowRight, contentDescription = "마감기한 선택")
+        }
     }
 }
+
