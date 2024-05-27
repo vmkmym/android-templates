ba25002c12da57b22f28b34f6af04bd465bdc87c
상은 <71699054+vmkmym@users.noreply.github.com>
2024-05-28 01:54:02 +0900

    feat: add view/SettingScreen

---
+++ app/src/main/java/com/example/composeproject/view/SettingScreen.kt
@@ -0,0 +1,12 @@
+package com.example.composeproject.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SettingScreen(
+    onHomeClick: () -> Unit,
+    onHabitDetailClick: () -> Unit,
+    onProgressClick: () -> Unit
+) {
+
+}
