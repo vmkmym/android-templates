1a67f5bd968aa5654f4992743a4bb90a18e617cd
상은 <71699054+vmkmym@users.noreply.github.com>
2024-05-28 01:54:57 +0900

    feat: add view/HabitDetailScreen

---
+++ app/src/main/java/com/example/composeproject/view/HabitDetailScreen.kt
@@ -0,0 +1,12 @@
+package com.example.composeproject.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HabitDetailScreen(
+    onHomeClick: () -> Unit,
+    onProgressClick: () -> Unit,
+    onSettingClick: () -> Unit
+) {
+
+}
