ddc22c4b40d021bf0c8e199aee3e37952fd2847f
상은 <71699054+vmkmym@users.noreply.github.com>
2024-05-28 01:54:15 +0900

    feat: add view/HabitProgressScreen

---
+++ app/src/main/java/com/example/composeproject/view/HabitProgressScreen.kt
@@ -0,0 +1,12 @@
+package com.example.composeproject.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HabitProgressScreen(
+    onHomeClick: () -> Unit,
+    onHabitDetailClick: () -> Unit,
+    onSettingClick: () -> Unit
+) {
+
+}
