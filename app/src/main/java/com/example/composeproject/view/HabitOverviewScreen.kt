1442ff34edcf52da0635de5156f122733d701230
상은 <71699054+vmkmym@users.noreply.github.com>
2024-05-28 01:54:37 +0900

    feat: add view/HabitOverviewScreen

---
+++ app/src/main/java/com/example/composeproject/view/HabitOverviewScreen.kt
@@ -0,0 +1,13 @@
+package com.example.composeproject.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HabitOverviewScreen(
+    onCreateNewHabitClick: () -> Unit,
+    onHabitDetailClick: () -> Unit,
+    onProgressClick: () -> Unit,
+    onSettingClick: () -> Unit
+) {
+
+}
